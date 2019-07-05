f3ebbf89bda344f178ffc88c65d046d343140e27
onikili <52492074+onikili@users.noreply.github.com>
2019-07-05 19:04:58 +0800

    Add files via upload

---
+++ 7月4日作业电路/dian_lu.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);//
+}
+void loop()
+{
+  digitalWrite(5,High);
+  digitalWrite(4，High);
+  digitalWrite(7,High);//
+  digitalWrite(6,High);
+  delay(10);
+}
